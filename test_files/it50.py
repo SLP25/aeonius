54e1e7546a597d07984172feaabf870149742825
luismfp <lumafepe@gmail.com>
2023-05-07 14:37:43 +0100

    50 questoes de pf

--- test_files/it50.py
+++ test_files/it50.py
@@ -91,4 +91,169 @@ def elemIndices(a,l):
 #20
 def nub(l):
     return list(set(l))
-            
+
+#21
+def delete(x,l):
+    l.remove(x)
+    return l
+
+#22
+def deleteAll(l1,l2):
+    for i in l2:
+        l1.remove(i)
+    return l1
+
+#23
+def union(a,b):
+    c=[]+a
+    for i in b:
+        if i not in a:
+            c.append(i)
+    return c
+
+#25
+def intersectAux(a,b):
+    for i in a:
+        if i in b:
+            yield i
+
+#24
+def intersect(a,b):
+    return list(intersectAux(a, b))
+
+#25
+def insert(a,l):
+    if l==[]:return [a]
+    h,*t=l
+    if h>a:return [a,h]+t
+    else: return [h]+insert(a,t)
+    
+#26
+def unwords(l):
+    return " ".join(l)
+
+#27
+def unlines(l):
+    return "\n".join(l)
+
+
+#28
+def maxx(l):
+    return max(l)
+
+def pMaior(l):
+    return max(enumerate(l),key=lambda x:x[1])[0]
+
+
+#29
+def temRepetidos(x):
+    return x!=list(set(x))
+
+#30
+def algarismos(x):
+    return list(filter(lambda y:y.isnumeric(),x))
+
+#31
+def posImpares(l):
+    return l[1::2]
+
+#32
+def posPares(l):
+    return l[0::2]
+#33
+def iSorted(l):
+    return l.sorted()
+
+#34
+def iSort(l):
+    return sorted(l)
+#35
+def menor(s1,s2):
+    return s1<s2
+#36
+def elemMSet(a,l):
+    return a in map(lambda x:x[0],l)
+#37
+def lengthMSet(a,l):
+    return sum(map(lambda x:x[1],l))
+
+def converteMSetAux(l):
+    for i in l:
+        for j in range(i[1]):
+            yield i[0]
+#38
+def converteMSet(l):
+    return list(converteMSetAux(l))
+#39
+def insereMSet(a,l):    
+    if l==[]: return [(a,1)]
+    h,*t=l
+    if h[0]==a: return [(a,h[1] + 1)]+t
+    return [h] + insereMSet(a,t)
+#40
+def removeMSet(a,l):
+    if l==[]:return []
+    h,*t=l
+    if h[0]==a: return t if h[1] == 1 else [(a, h[1] - 1)]+t
+    return [h] + removeMSet(a,t)
+#41
+def constroiMSetAux(l):
+    for i in set(l):
+        yield (i,l.count(i))
+        
+def constroiMSet(l):
+    return list(constroiMSetAux(l))
+#42
+def partitionEithers(l):
+    r={"a":[],"b":[]}
+    for i in l:
+        for k,v in i.items():
+            r[k].append(v)
+    return (r['a'],r['b'])
+
+#43
+# Maybe => Valor ou None
+
+def catMaybes(l):
+    return list(filter(lambda x:x!=None, l))
+
+#44
+# Posicoes sao strings
+
+def posicao(xy,l):
+    x,y=xy
+    if l==[]:return (x,y)
+    for p in l:
+        if p=="Norte": y+=1
+        elif p=="Sul": y-=1
+        elif p=="Oeste":x -= 1
+        elif p=="Este":x += 1
+    return (x,y)
+
+#45
+def caminho(p1,p2):
+    x1,y1=p1
+    x2, y2=p2
+    norm=lambda x:0 if x<0 else x
+    r=[]
+    r+=['Este']*norm(x2 - x1)
+    r+=['Oeste']*norm(x1 - x2)
+    r+=['Norte']*norm(y2 - y1)
+    r+=['Sul']*norm(y1 - y2)
+    return r
+
+#46
+def vertical():
+    return len(filter(lambda y: y == "Este" or y == "Oeste",l)) == 0
+#47
+def maisCentral(l):
+    return min(l,key=lambda x: (x['x'] ^ 2) + (x['y'] ^ 2))
+#48
+def vizinhos(p,l):
+    return list(filter(lambda h:(abs(p['x']-h['x']) + abs(p['y']-h['y'])) <= 1,l))
+#49
+def mesmaOrdenada(l):
+    return len(set(map(lambda x:x['y'],l)))==1
+#50
+def interseccaoOk(l):
+    return len(filter(lambda s: s != "Vermelho",l)) <= 1
--- test_files/rec50.py
+++ test_files/rec50.py
@@ -1,3 +1,11 @@
+def negate(b):
+    return False if b else True
+
+def find(a,l):
+    h,*t=l
+    if a == h:return 0
+    return 1 + find(a,t) 
+
 #1
 def enumFromTo(f,t):
     if t-f==0: return [f]
@@ -130,3 +138,235 @@ def nub(l):
             if elem(x,ls): return nubb(xs,ls)
             else: [x]+nubb(xs,[x]+ls)            
     return nubb(l,[])
+
+#21
+def delete(x,l):
+    if l==[]:return []
+    h,*t=l
+    return t if h==x else [h]+delete(x,t)
+
+
+#22
+def deleteAll(l1,l2):
+    if l2==[]:return l1
+    h,*t = l2
+    return deleteAll(delete(h,l1),t)
+
+#23
+def union(a,b):
+    return a + list(filter(lambda x: negate(elem(x, a)),b))
+
+#24
+def intersect(a,b):
+    return list(filter(lambda x: elem(x,b),a))
+
+#25
+def insert(a,l):
+    if l==[]:return [a]
+    h,*t=l
+    if h>a:return [a,h]+t
+    else: return [h]+insert(a,t)
+
+#26
+def unwords(l):
+    return concat(intersperce(" ",l))
+
+#27
+def unlines(l):
+    return concat(intersperce("\n",l))
+
+
+#28
+def max(l):
+    h,*t=l
+    if l==[h]:return [h]
+    else:
+        m1=max(t)
+        return m1 if m1 > h else h
+
+def pMaior(l):
+    return find(max(l),l)
+
+
+#29
+def temRepetidos(x):
+    return x == nub(x)
+
+#30
+def algarismos(x):
+    return list(filter(lambda y:y.isnumeric(),x))
+
+#31
+def posImpares(l):
+    if len(l)<2:
+        return []
+    else:
+        h1,h2,*t = l
+        return [h2] + posImpares(t)
+
+
+#32
+def posPares(l):
+    if l==[]:return []
+    h1,*t=l
+    if l==[h1]:return [h1]
+    h2,*t=t
+    return [h1] + posPares(t)
+
+
+#33
+def iSorted(l):
+    if len(l)<2:return True
+    h1,h2,*t = l
+    return iSorted([h2]+t) if h1 <= h2 else False
+
+#34
+def iSort(l):
+    if l==[]:return []
+    h,*t=l
+    return insert(h,iSort(t))
+
+#35
+def menor(s1,s2):
+    if s1=="":
+        if s2=="":return False
+        else :return True
+    h,*t = s1
+    if s2 == "": return False
+    h1,*t1 =s2
+    if ord(h) > ord(h1):return False
+    elif ord(h) < rd(h1):return True
+    return menor(concat(t),concat(t1))
+
+#36
+def elemMSet(a,l):
+    if l==[]:return False
+    (b,_),*t = l
+    if a==b: return True
+    else: return elemMSet(a, t)
+
+#37
+def lengthMSet(l):
+    if l==[]:return 0
+    (_,x),*t=l
+    return x + lengthMSet(t)
+
+
+#38
+def converteMSet(l):
+    if l==[]:return []
+    (a,x),*t=l
+    return replicate(x,a) + converteMSet(t)
+
+
+#39
+def insereMSet(a,l):
+    if l==[]: return [(a,1)]
+    h,*t=l
+    if h[0]==a: return [(a,h[1] + 1)]+t
+    return [h] + insereMSet(a,t)
+
+
+#40
+def removeMSet(a,l):
+    if l==[]:return []
+    h,*t=l
+    if h[0]==a: return t if h[1] == 1 else [(a, h[1] - 1)]+t
+    return [h] + removeMSet(a,t)
+
+#41
+def constroiMSet(l):
+    gl=group(l)
+    return list(map(lambda x:(x[0],len(x)),gl))
+
+#42
+# Um either é um dicionario: {"a": valor} ou {"b": valor}
+
+def partitionEithers(l):
+    if l==[]:return ([],[])
+    h,*t =l
+    a1,b1 = partitionEithers(t)
+    if "a" in h:
+        x=h["a"]
+        return ([x]+a1, b1)
+    else: 
+        x=h["b"]
+        return (a1,[x] + b1)
+        
+
+
+#43
+# Maybe => Valor ou None
+
+def catMaybes(l):
+    if l==[]:return []
+    h,*t=l
+    if h==None: return catMaybes(t)
+    else:return [h] + catMaybes(t)
+
+
+#44
+# Posicoes sao strings
+
+def posicao(xy,l):
+    x,y=xy
+    if l==[]:return (x,y)
+    h,*t=l
+    if h=="Norte": posicao((x, y + 1),t)
+    elif h=="Sul": posicao((x, y - 1),t)
+    elif h=="Oeste": posicao((x - 1, y),t)
+    elif h=="Este": posicao((x + 1, y),t)
+    
+    
+#45
+def caminho(p1,p2):
+    x1,y1=p1
+    x2, y2=p2
+    if x1 <= x2 and y1 <= y2 :return replicate((x2 - x1),"Este") + replicate((y2 - y1) ,"Norte")
+    elif x1 <= x2 and y1 > y2  :return replicate((x2 - x1),"Este") + replicate((y1 - y2) ,"Sul")
+    elif x1 > x2 and y1 <= y2  :return replicate((x1 - x2),"Oeste") + replicate( (y2 - y1), "Norte")
+    elif x1 > x2 and y1 > y2   :return replicate((x1 - x2),"Oeste") + replicate( (y1 - y2), "Sul")
+
+#46
+def vertical():
+    return len(filter(lambda y: y == "Este" or y == "Oeste",l)) == 0
+
+#47
+# Estrutura é um dicionario {"x": x, "y": y}
+
+def maisCentral(l):
+    h,*t=l
+    if [h]==l:return h
+    rt=maisCentral(t)
+    x1=rt['x']
+    y1=rt['y']
+    x=h['x']
+    y=h['y']
+    d1 = (x1 ^ 2) + (y1 ^ 2)
+    d = (x ^ 2) + (y ^ 2)
+    return h if d <= d1 else rt
+        
+        
+#48
+
+def vizinhos(p,l):
+    if l==[]:return []
+    x,y=p['x'],p['y']
+    h,*t=l
+    x1,y1=h['x'],h['y']
+    t1 = vizinhos(p,t)
+    return t1 if ((abs(x-x1)) + (abs(y-y1))) > 1 else [h] + t1
+
+#49   
+def mesmaOrdenada(l):
+    if len(l)<2:return True
+    h1,h2,*t=l
+    y1,y2=h1['y'],h2['y']
+    if y1 != y2:return False
+    else: return mesmaOrdenada([h2]+t)
+
+
+
+#50
+def interseccaoOk(l):
+    return len(filter(lambda s: s != "Vermelho",l)) <= 1
